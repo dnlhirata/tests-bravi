529242e9e44b1642c98fdcd429d2ed51a5c89982
Daniel Hirata <daniel@directdigital.com.br>
2019-11-07 12:48:58 -0300

    Addded Balanced Brackets assignment

---
+++ balanced-brackets/balanced-brackets/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace balanced_brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidateBracketsOrder("{([])}()[][()][{]"));
+        }
+
+        private static bool ValidateBracketsOrder(string brackets)
+        {
+            if (String.IsNullOrEmpty(brackets))
+            {
+                return false;
+            }
+
+            var leftBrackets = new char[] {'{', '[', '('};
+            var rightBrackets = new char[] {'}', ']', ')'};
+            var bracketsCombinations = new Dictionary<char, char>
+            {
+                { '{', '}' },
+                { '[', ']' },
+                { '(', ')' },
+            };
+
+            var stack = new Stack<char>();
+
+            foreach (var ch in brackets.ToCharArray())
+            {
+
+                if (leftBrackets.Contains(ch))
+                    stack.Push(ch);
+
+                if (!rightBrackets.Contains(ch)) continue;
+
+                if (stack.Count == 0)
+                    return false;
+
+                var lastElement = stack.Peek();
+                if (bracketsCombinations[lastElement] == ch)
+                    stack.Pop();
+            }
+
+            return stack.Count == 0;
+        }
+    }
+
+
+}
